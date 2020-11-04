1de7e705d593fabec36b196df8343289c29017c8
magoninho <magoninhogamer@gmail.com>
2020-11-04 14:46:51 -0300

    quase lá

--- main.py
+++ main.py
@@ -1,58 +1,193 @@
 import pygame
-import sys
-from bird import *
-from consts import *
-from cano import *
+import os
+import random
+
+pygame.mixer.init(44000, -16, 1, 1024)
+pygame.mixer.music.load('song.ogg')
+pygame.mixer.music.play(-1)
+
+scores = [0]
 
 
-# display settings
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+def main():
 
-# condition to play+
-started = False
-pygame.font.init()
-font = pygame.font.SysFont("Arial", 32)
-text = font.render("Press up arrow to start", False, consts.BLACK)
+    # CONSTANTS
+    WIDTH, HEIGHT = 600, 600
+    SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 
+    # colors
+    # BLACK = (0, 0, 0)
+    # GREEN = (0, 255, 0)
+    # YELLOW = (245, 239, 66)
+    # CYAN = (151, 223, 228)
 
-# instanciar objetos
-player = Bird(screen, started)
+    # paleta de cores
 
-canos = []  # canos agrupados
+    cor = random.randint(0, 4)
+    """
+        background color first
+        pipe color second
+        bird color third
+    """
+    cores = [
 
-x = consts.WIDTH + consts.DISTANCIA_INICIAL_DO_CANO
-for i in range(consts.PIPE_NUMBER):
-    cano = Cano(screen, x)
-    canos.append(cano)
-    x += consts.WIDTH/consts.PIPE_NUMBER + 40
-    print(canos)
+        [
+            (76, 111, 255),   # COOL_BLUE
+            (151, 223, 228),  # CYAN
+            (255, 255, 255),  # WHITE
+        ],
+        [
+            (0, 0, 0),   # COOL_BLUE
+            (255, 255, 255),  # CYAN
+            (255, 0, 0),  # WHITE
+        ],
+        [
+            (34, 37, 159),   # DARK_BLUE
+            (85, 165, 255),  # BLUE
+            (162, 234, 0)    # LIGHT GREEN
+        ],
+        [
+            (250, 92, 101),  # LIGHT RED
+            (55, 220, 148),  # LIMA
+            (255, 255, 255)  # WHITE
+        ],
+        [
+            (245, 233, 103),  # YELLOW
+            (160, 143, 213),  # LIGHT PURPLE
+            (255, 255, 255)  # WHITE
+        ]
+    ]
 
+    # stuff #
+    PIPE_NUMBER = 2
 
-# game loop
-while True:
-    clock = pygame.time.Clock()
-    clock.tick(60)
+    ## CLASSES ##
 
-    for event in pygame.event.get():
-        # clicar no x
-        if event.type == pygame.QUIT:
-            pygame.quit()
+    class Score:
+        def __init__(self):
+            self.x = 10
+            self.y = 10
 
-        # apertar esc
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+        def draw(self):
+            text = str(bird.score)
+            pygame.font.init()
+            font = pygame.font.SysFont('', 72)
+            text = font.render(text, False, cores[cor][2])
+            SCREEN.blit(text, (WIDTH/2-10, self.y))
+
+        def draw_last_score(self):
+            text = f"BEST: {bird.high_score[-1]}"
+            pygame.font.init()
+            font = pygame.font.SysFont('', 32)
+            texto = font.render(text, False, cores[cor][2])
+            SCREEN.blit(texto, (self.x, self.y))
+    # THE BIRD
+
+    class Bird:
+        def __init__(self):
+            self.x = WIDTH/4
+            self.y = HEIGHT/2 - 20
+            self.velocity = 0
+            self.gravity = 1
+            self.score = 0
+            self.high_score = sorted(scores)
+
+        def draw_bird(self):
+            self.rect = pygame.Rect(self.x, self.y, 40, 40)
+            pygame.draw.rect(SCREEN, cores[cor][2], self.rect)
+
+        def update(self):  # make the bird fall, controllers etc
+
+            self.draw_bird()
+            self.y += self.velocity
+            self.velocity += self.gravity  # aceleração
+
+            # controles #
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_UP]:
+                self.velocity = -10
+
+            if self.y < 0 or self.y > HEIGHT:
+                scores.append(bird.score)
+                main()  # reseta do jogo chamado a função main
+
+            for i in range(len(canos)):  # colisões em todas os canos da lista
+                if self.rect.colliderect(canos[i].rect_top) or self.rect.colliderect(canos[i].rect_bottom):
+                    scores.append(bird.score)
+                    main()  # reseta do jogo chamado a função main
+
+    # obstacle
+
+    class Obstacle:
+        def __init__(self, x):
+            self.x = x
+            self.y = random.randint(-300, 0)
+
+            self.space_between = 570
+            self.rect_top = pygame.Rect(self.x, self.y, 80, 400)
+            self.rect_bottom = pygame.Rect(
+                self.x, self.y + self.space_between, 80, 400)
+
+            self.pipe_vel = 9
+
+        def draw(self):
+            self.rect_top.x -= self.pipe_vel
+            self.rect_bottom.x -= self.pipe_vel
+            pygame.draw.rect(SCREEN, cores[cor][1], self.rect_top)
+            pygame.draw.rect(SCREEN, cores[cor][1], self.rect_bottom)
+            if self.rect_top.x < -80:
+                self.rect_top.x = WIDTH
+                self.rect_bottom.x = WIDTH
+                self.rect_top.y = random.randint(-300, 0)
+                self.rect_bottom.y = self.rect_top.y + 570
+                bird.score += 1
+
+    # function
+    # Limpador de tela multiplataforma Magoninho Gamer versão 1.2
+
+    def limpa_cosole():
+        os.system('cls' if os.name == 'nt' else 'clear')
+    # limpa_cosole()
+    # object instances
+    bird = Bird()
+    obj_score = Score()
+
+    canos = []
+
+    x = WIDTH
+    for i in range(PIPE_NUMBER):
+        x += WIDTH/PIPE_NUMBER + 40
+        obstacle = Obstacle(x)
+        canos.append(obstacle)
+
+    # GAME LOOP
+    while True:
+        clock = pygame.time.Clock()
+        clock.tick(70)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
 
-    screen.fill(CYAN)
-    # player.draw_dino()
-    player.update(started)
-    if not player.started:
-        screen.blit(text, (consts.WIDTH / 6, consts.HEIGHT / 1.5))
-    for x in range(consts.PIPE_NUMBER):
-        if player.started:
-            teste = canos[x].draw(player.started, player)
-            print("started")
-        else:
-            canos[x].x = consts.WIDTH
-    # display update
-    pygame.display.update()
+        SCREEN.fill(cores[cor][0])
+        # drawinings
+        bird.draw_bird()
+        bird.update()
+        for cano in range(PIPE_NUMBER):
+            canos[cano].draw()
+        obj_score.draw()
+        obj_score.draw_last_score()
+
+        pygame.display.update()
+
+
+main()
+
+
+"""
+Feito com carinho
+pelo Magoninho :D
+"""
